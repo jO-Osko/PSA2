761f96b6448faafc1bbe89833b0a40feafc61b5a
Žiga Zupančič <zigazupancic5@gmail.com>
2017-04-16 23:47:21 +0200

    Dodaj brisanje elementov.

--- naloge/dn1/tree/ZigaZupancic/BTree.py
+++ naloge/dn1/tree/ZigaZupancic/BTree.py
@@ -28,6 +28,8 @@ class BTree(AbstractTree):
         :param force: True, če vstavljamo direktno v izbrano vozlišče, tudi če ima otroke, sicer False
         :return: None
         """
+        if self.search(item):
+            return None
         left_child = None
         right_child = None
         if node is None:
@@ -97,13 +99,85 @@ class BTree(AbstractTree):
         if force:
             return left_child, right_child
 
-    def remove(self, item):
+    def remove(self, item, force=False):
         """
         Odstrani element item iz iskalnega drevesa, če ga ni naj metoda sproži ValueError
         :param item: Element, ki ga brišemo iz drevesa
         :return: None
         """
-        raise NotImplementedError("Not implemented")
+        if not self.search(item):
+            raise ValueError("Item not in tree")
+        node = self.root
+        while item not in node.keys:
+            index = bisect.bisect(node.keys, item)
+            node = node.children[index]
+        if node.children is None or force:
+            if len(node.keys) == self.order and self.root is not node:
+                index = bisect.bisect(node.parent.keys, node.keys[0])
+                if len(node.parent.keys) > index and len(node.parent.children[index + 1].keys) > self.order:
+                    # Rotacija levo
+                    right_sibling = node.parent.children[index + 1]
+                    node.keys.append(node.parent.keys[index])
+                    node.parent.keys[index] = right_sibling.keys[0]
+                    right_sibling.keys = right_sibling.keys[1:]
+                    node.keys.remove(item)
+                elif index > 0 and len(node.parent.children[index - 1].keys) > self.order:
+                    # Rotacija desno
+                    left_sibling = node.parent.children[index - 1]
+                    node.keys.insert(0, node.parent.keys[index])
+                    node.parent.keys[index] = left_sibling.keys[-1]
+                    left_sibling.keys = left_sibling.keys[:-1]
+                    node.keys.remove(item)
+                else:
+                    # Spoji s sorojencem in vmes postavi ločno vrednost iz straša
+                    if index > 0:
+                        left = node.parent.children[index - 1]
+                        right = node
+                    else:
+                        left = node
+                        right = node.parent.children[index + 1]
+                    left.keys.append(node.parent.keys[index - 1])
+                    left.keys += right.keys
+                    left.keys.remove(item)
+                    if force:
+                        left.children += right.children
+                        for child in right.children:
+                            child.parent = left
+                    node.parent.children.remove(right)
+                    if left.parent.parent is None and len(left.parent.keys) == 1:
+                        # Smo v korenu in koren ima le en ključ
+                        self.root = left
+                        left.parent = None
+                    else:
+                        self.remove(node.parent.keys[index - 1], force=True)
+            else:
+                node.keys.remove(item)
+        else:
+            min_leaf = node.children[bisect.bisect_right(node.keys, item)]
+            while min_leaf.children is not None:
+                min_leaf = min_leaf.children[0]
+            index = bisect.bisect_left(node.keys, item)
+            min_element = min_leaf.keys[0]
+            self.remove(min_element)
+            self.replace(item, min_element)
+
+    def replace(self, item1, item2, node=None):
+        """
+        Najde element item1, če obstaja ter ga zamenja z item2, če je to možno.
+        :param item1: Element, ki ga želimo zamenjati
+        :param item2: Element s katerim ga želimo zamenjati
+        :param node: Vozišče iz katerega iščemo
+        :return: True, če je izmenjava možna in se je izvršila, drugače False.
+        """
+        if node is None:
+            node = self.root
+        pos = bisect.bisect(node.keys, item1)
+        if pos > 0 and item1 == node.keys[pos - 1]:
+            node.keys[pos - 1] = item2
+            return True
+        elif node.children is not None:
+            return self.replace(item1, item2, node.children[pos])
+        return False
 
     def search(self, item, node=None):
         """
