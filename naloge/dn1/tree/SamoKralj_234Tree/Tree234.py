b83586be21412ed0d10aaf372cd4ea9731dbb5a8
Samo Kralj <samokralj95@gmail.com>
2017-04-17 19:15:10 +0200

    Delujoce pravilno vstavljanje v drevo

--- naloge/dn1/tree/SamoKralj_234Tree/Tree234.py
+++ naloge/dn1/tree/SamoKralj_234Tree/Tree234.py
@@ -26,13 +26,70 @@ class Tree_234(AbstractTree):
         return find(self, k, self.koren)
         
 class Node():
-    def __init__(self, value = None):
-        self.key = [('V', value)] if value is not None else []
-        self.parent = None
-        self.depth = 0
+    def __init__(self, value = None, parent = None):
+        self.keys = []
+        self.childs = [None]
+        self.parent = parent
+        
 
     def __repr__(self):
-        return str(self.key)
+        return 'Keys: ' + str(self.keys) + ' Childs: ' + str(self.childs)
+
+    def indeks(self, k):
+        for i in range(len(self.keys)):
+            if self.keys[i] == k:
+                return i
+        return False
+
+    def add(self, value, repair = True):
+        i = 0
+        while i < len(self.keys) and self.keys[i] < value:
+            i = i + 1
+        self.keys = self.keys[:i] + [value] + self.keys[i:]
+        if self.childs[i] is None:
+            self.childs = self.childs[:i] + [None] + self.childs[i:]
+        else:
+            if self.childs[i].keys[0] > value:
+                self.childs = self.childs[:i] + [None] + self.childs[i:]
+            else:
+                self.childs = self.childs[:i+1] + [None] + self.childs[i+1:]
+        if repair:
+            self.repair()
+
+    def repair(self):
+        if len(self.keys) > 3:
+            print(self)
+            pushup = self.keys[2]
+            #LEVO = Node()
+            #LEVO.keys = self.keys[:2][:]
+            #LEVO.childs = self.childs[:3][:]
+            DESNO = Node()
+            DESNO.keys = self.keys[3:][:]
+            DESNO.childs = self.childs[3:][:]
+            self.keys = self.keys[:2][:]
+            self.childs = self.childs[:3][:]
+            print(self)
+            print(DESNO)
+            if self.parent is not None:
+                #LEVO.parent = self.parent
+                DESNO.parent = self.parent
+                self.parent.add(pushup, repair = False)
+                ind = self.parent.indeks(pushup)
+                self.parent.childs[ind] = self
+                self.parent.childs[ind+1] = DESNO
+            else:
+                parent = Node()
+                self.parent = parent
+                #LEVO.parent = parent
+                DESNO.parent = parent
+                parent.add(pushup, repair = False)
+                parent.childs[0] = self
+                parent.childs[1] = DESNO
+            self.parent.repair()
+            
+
+    def remove(self, value):
+        pass
                         
                 
             
