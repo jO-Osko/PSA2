a20e6bf6de1cba4917ad28cc852aa1097193f78e
Luka Avbreht <l.avbreht@gmail.com>
2017-05-04 11:05:50 +0200

    Dodani testi za profesorja

--- naloge/dn1/test_za_profesorja.py
+++ naloge/dn1/test_za_profesorja.py
@@ -1,5 +1,23 @@
 from users import import_helper,trees
 
+
+
+from tree.FilipKoprivec.RedBlackTree import RedBlackTree
+from tree.EvaErzin.SplayTree import SplayTree
+from tree.SamoKralj_234Tree.Tree234 import Tree_234
+from tree.ZigaZupancic.BTree import BTree
+from tree.KevinStampar.Treap import Treap
+from tree.LukaAvbreht.AvlTree import AvlTree
+from tree.NinaSlivnik.MyList import SkipList
+from tree.Jan_Golob.BplusTree import BplusTree
+from tree.LukaLajovic.ScapeGoat import ScapeGoat
+
+print("Drevesa, ki jih lahko uporabljamo")
 for tree in trees:
-	import_helper(tree)
+	print("{0} ki ga uporabimo kot tree = {0}()".format(tree[2]))
+
+	
+
+	
+	
 	
